381918704dc76572d0d7d020f43510dba8fa3b14
FSPonte <fsponte@outlook.com>
2024-05-31 16:28:04 +0100

    Wrapper for all libraries

---
+++ include/pix_lib.hpp
@@ -0,0 +1,24 @@
+#ifndef _PIX_LIB_HPP_
+#define _PIX_LIB_HPP_
+
+/**
+ * Pi-X Namespace
+ * pix
+*/
+#include <type_info.hpp>
+#include <smart_pointer.hpp>
+
+/**
+ * Pi-X Abstract Data Types
+ * pix::adt
+*/
+#include <string.hpp>
+#include <array.hpp>
+#include <stack.hpp>
+#include <queue.hpp>
+#include <deque.hpp>
+#include <hash_table.hpp>
+#include <vector.hpp>
+#include <matrix.hpp>
+
+#endif // _PIX_LIB_HPP_
