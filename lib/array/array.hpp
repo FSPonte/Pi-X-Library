d7b16ad7dc3e8e72ab67c1339122de187247f4d6
FSPonte <fsponte@outlook.com>
2024-06-07 04:33:45 +0100

    Visual changes

--- lib/array/array.hpp
+++ lib/array/array.hpp
@@ -24,25 +24,9 @@ namespace pix::adt
 
         /**
          * @brief Parameterized construtor
-         * @tparam _type_t Data type
          * @param arr Pointer to array
         */
-        template <typename _type_t>
-        array(const _type_t*);
-
-        /**
-         * @brief Parameterized construtor
-         * @param arr Pointer to array
-        */
-        array(const type_t*);
-
-        /**
-         * @brief Copy constructor
-         * @tparam _type_t Data type
-         * @param arr Array
-        */
-        template <typename _type_t>
-        array(const array<_type_t, DIM>&);
+        array(const type_t[]);
 
         /**
          * @brief Copy constructor
@@ -72,27 +56,10 @@ namespace pix::adt
 
         /**
          * @brief Copy operator
-         * @tparam _type_t Data type
          * @param arr Array
          * @note Assumes same dimensions as object
         */
-        template <typename _type_t>
-        void operator = (const _type_t*);
-
-        /**
-         * @brief Copy operator
-         * @param arr Array
-         * @note Assumes same dimensions as object
-        */
-        void operator = (const type_t*);
-
-        /**
-         * @brief Copy operator
-         * @tparam _type_t Data type
-         * @param arr Array
-        */
-        template <typename _type_t>
-        void operator = (const array<_type_t, DIM>&);
+        void operator = (const type_t[]);
 
         /**
          * @brief Copy operator
@@ -102,31 +69,21 @@ namespace pix::adt
 
         /**
          * @brief Equality operator
-         * @tparam _type_t Data type
          * @param arr C styled array
          * @return True if arrays are equal
+         * @note Assumes same dimensions as object
+        */
+        const bool operator == (const type_t[]) const;
+
+        /**
+         * @brief Equality operator
+         * @tparam _type_t Data type
+         * @param arr C styled array
+         * @return False
          * @note Assumes same dimensions as object
         */
         template <typename _type_t>
-        constexpr const bool operator == (const _type_t*) const;
-
-        /**
-         * @brief Equality operator
-         * @param arr C styled array
-         * @return True if arrays are equal
-         * @note Assumes same dimensions as object
-        */
-        const bool operator == (const type_t*) const;
-
-        /**
-         * @brief Equality operator
-         * @tparam _type_t Data type
-         * @tparam _DIM Number of elements
-         * @param arr Array
-         * @return True if arrays are equal
-        */
-        template <typename _type_t, unsigned long _DIM>
-        constexpr const bool operator == (const array<_type_t, _DIM>&) const;
+        constexpr const bool operator == (const _type_t[]) const;
 
         /**
          * @brief Equality operator
@@ -136,31 +93,32 @@ namespace pix::adt
         const bool operator == (const array<type_t, DIM>&) const;
 
         /**
+         * @brief Equality operator
+         * @tparam _type_t Data type
+         * @tparam _DIM Number of elements
+         * @param arr Array
+         * @return False
+        */
+        template <typename _type_t, unsigned long _DIM>
+        constexpr const bool operator == (const array<_type_t, _DIM>&) const;
+
+        /**
+         * @brief Inequality operator
+         * @param arr C styled array
+         * @return True if arrays are different
+         * @note Assumes same dimensions as object
+        */
+        const bool operator != (const type_t[]) const;
+
+        /**
          * @brief Inequality operator
          * @tparam _type_t Data type
          * @param arr C styled array
-         * @return True if arrays are different
+         * @return True
          * @note Assumes same dimensions as object
         */
         template <typename _type_t>
-        constexpr const bool operator != (const _type_t*) const;
-
-        /**
-         * @brief Inequality operator
-         * @param arr C styled array
-         * @return True if arrays are different
-         * @note Assumes same dimensions as object
-        */
-        const bool operator != (const type_t*) const;
-
-        /**
-         * @brief Inequality operator
-         * @tparam _type_t Data type
-         * @param arr Array
-         * @return True if arrays are different
-        */
-        template <typename _type_t, unsigned long _DIM>
-        constexpr const bool operator != (const array<_type_t, _DIM>&) const;
+        constexpr const bool operator != (const _type_t[]) const;
 
         /**
          * @brief Inequality operator
@@ -168,6 +126,15 @@ namespace pix::adt
          * @return True if arrays are different
         */
         const bool operator != (const array<type_t, DIM>&) const;
+
+        /**
+         * @brief Inequality operator
+         * @tparam _type_t Data type
+         * @param arr Array
+         * @return True
+        */
+        template <typename _type_t, unsigned long _DIM>
+        constexpr const bool operator != (const array<_type_t, _DIM>&) const;
     };
 }
 
--- lib/array/array.ipp
+++ lib/array/array.ipp
@@ -14,17 +14,7 @@ namespace pix::adt
     }
 
     template <typename type_t, unsigned long DIM>
-    template <typename _type_t>
-    array<type_t, DIM>::array(const _type_t* arr) : array<type_t, DIM>()
-    { *this = arr; }
-
-    template <typename type_t, unsigned long DIM>
-    array<type_t, DIM>::array(const type_t* arr) : array<type_t, DIM>()
-    { *this = arr; }
-
-    template <typename type_t, unsigned long DIM>
-    template <typename _type_t>
-    array<type_t, DIM>::array(const array<_type_t, DIM>& arr) : array<type_t, DIM>()
+    array<type_t, DIM>::array(const type_t arr[]) : array<type_t, DIM>()
     { *this = arr; }
 
     template <typename type_t, unsigned long DIM>
@@ -44,18 +34,7 @@ namespace pix::adt
     { return DIM; }
 
     template <typename type_t, unsigned long DIM>
-    template <typename _type_t>
-    void array<type_t, DIM>::operator = (const _type_t* arr)
-    {
-        if (arr == nullptr)
-            return;
-
-        for (unsigned long i = 0; i < DIM; ++i)
-            this->data[i] = static_cast<type_t>(arr[i]);
-    }
-
-    template <typename type_t, unsigned long DIM>
-    void array<type_t, DIM>::operator = (const type_t* arr)
+    void array<type_t, DIM>::operator = (const type_t arr[])
     {
         if (arr == nullptr)
             return;
@@ -65,21 +44,11 @@ namespace pix::adt
     }
 
     template <typename type_t, unsigned long DIM>
-    template <typename _type_t>
-    void array<type_t, DIM>::operator = (const array<_type_t, DIM>& arr)
-    { *this = arr.buffer(); }
-
-    template <typename type_t, unsigned long DIM>
     void array<type_t, DIM>::operator = (const array<type_t, DIM>& arr)
     { *this = arr.buffer(); }
 
     template <typename type_t, unsigned long DIM>
-    template <typename _type_t>
-    constexpr const bool array<type_t, DIM>::operator == (const _type_t* arr) const
-    { return false; }
-
-    template <typename type_t, unsigned long DIM>
-    const bool array<type_t, DIM>::operator == (const type_t* arr) const
+    const bool array<type_t, DIM>::operator == (const type_t arr[]) const
     {
         if (arr == nullptr)
             return false;
@@ -94,8 +63,8 @@ namespace pix::adt
     }
 
     template <typename type_t, unsigned long DIM>
-    template <typename _type_t, unsigned long _DIM>
-    constexpr const bool array<type_t, DIM>::operator == (const array<_type_t, _DIM>& arr) const
+    template <typename _type_t>
+    constexpr const bool array<type_t, DIM>::operator == (const _type_t arr[]) const
     { return false; }
 
     template <typename type_t, unsigned long DIM>
@@ -103,22 +72,27 @@ namespace pix::adt
     { return *this == arr.buffer(); }
 
     template <typename type_t, unsigned long DIM>
-    template <typename _type_t>
-    constexpr const bool array<type_t, DIM>::operator != (const _type_t* arr) const
-    { return true; }
+    template <typename _type_t, unsigned long _DIM>
+    constexpr const bool array<type_t, DIM>::operator == (const array<_type_t, _DIM>& arr) const
+    { return false; }
 
     template <typename type_t, unsigned long DIM>
-    const bool array<type_t, DIM>::operator != (const type_t* arr) const
+    const bool array<type_t, DIM>::operator != (const type_t arr[]) const
     { return !(*this == arr); }
 
     template <typename type_t, unsigned long DIM>
-    template <typename _type_t, unsigned long _DIM>
-    constexpr const bool array<type_t, DIM>::operator != (const array<_type_t, _DIM>& arr) const
+    template <typename _type_t>
+    constexpr const bool array<type_t, DIM>::operator != (const _type_t arr[]) const
     { return true; }
 
     template <typename type_t, unsigned long DIM>
     const bool array<type_t, DIM>::operator != (const array<type_t, DIM>& arr) const
     { return !(*this == arr.buffer()); }
+
+    template <typename type_t, unsigned long DIM>
+    template <typename _type_t, unsigned long _DIM>
+    constexpr const bool array<type_t, DIM>::operator != (const array<_type_t, _DIM>& arr) const
+    { return true; }
 }
 
 #endif // _ARRAY_IPP_
