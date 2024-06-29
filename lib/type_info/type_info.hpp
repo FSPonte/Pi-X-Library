825a67d98883454223fcec2b01716eed1689c951
FSPonte <fsponte@outlook.com>
2024-06-29 14:55:27 +0100

    Fixed a typo

--- lib/type_info/type_info.hpp
+++ lib/type_info/type_info.hpp
@@ -62,7 +62,7 @@ static_assert                            \
  * @param type Data type
  * @return True if is a primitive floating point numerical data type
 */
-#define is_float(type) is_float_point<type>::value
+#define is_float(type) is_float<type>::value
 
 /**
  * @brief Static assertion for is_float
