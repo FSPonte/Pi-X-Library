ab980852345f9d38723cdbe6f6e9d6fd77a39326
FSPonte <fsponte@outlook.com>
2024-11-27 00:16:10 +0000

    Updated the test file

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,6 @@
 // Pi-X library
 #include <pix_lib.hpp>
 
-#include <cmath>
-
 /**
  * @brief Pause the process
  * @param msg Message
@@ -17,41 +15,15 @@ void pause(const char[] = nullptr);
 
 int main(int argc, char* argv[])
 {
-    auto PI = pix::constants::mathematics::PI;
-
     // Print arguments
     {
         std::cout << "Arguments:" << '\n';
 
-        for (unsigned long i = 0; i < argc; ++i)
+        for (pix::types::size_t i = 0; i < argc; ++i)
             std::cout << '\t' << argv[i] << '\n';
 
         std::cout << '\n';
     }
-
-    long double arg;
-
-    try
-    {
-        for (unsigned long i = 0; i < 1E3; ++i)
-        {
-            arg = pix::random::drand(- 20 * PI, 20 * PI);
-
-            std::cout
-                << "arg = " << arg << '\n'
-                << "tan = " << pix::math::trig::tan(arg) << '\n';
-            
-            arg = pix::math::trig::arctan(pix::math::trig::tan(arg));
-
-            std::cout
-                << "arg = " << arg << '\n'
-                << "tan = " << pix::math::trig::tan(arg) << '\n';
-
-            std::cout << '\n';
-        }
-    }
-    catch (const char err_msg[])
-    { std::cout << err_msg << '\n'; }
 
     return EXIT_SUCCESS;
 }
