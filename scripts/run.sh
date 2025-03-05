973e7c73828383c710dc25fe53889d4ab32fe325
FSPonte <fsponte@outlook.com>
2025-03-05 11:14:34 +0000

    Visual changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,3 @@
 sh scripts/build.sh
-echo
-time -f "\nTime (s):\n\treal: %e\n\tuser: %U\n\tsys: %S\n\nMemory (KB):\n\tmax: %M\n\tavg: %t\n\tavg total: %K" \
-	build/pix_tester | tee logs/terminal_log.txt
+time -v build/pix_tester | tee logs/terminal_log.txt
 open files/plot.png
