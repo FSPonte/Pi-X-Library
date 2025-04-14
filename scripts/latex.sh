d0d847b077e4d891676ede5df22b8f536e760985
FSPonte <fsponte@outlook.com>
2025-04-14 23:52:55 +0100

    Added a script for compiling LaTeX files to PDF

---
+++ scripts/latex.sh
@@ -0,0 +1,20 @@
+echo -n "File name: "
+read file_name
+
+pdflatex -output-directory="doc/" doc/$file_name
+
+if [ -f "doc/texput.log" ]; then
+    rm "doc/texput.log"
+fi
+
+if [ -f "doc/$file_name.aux" ]; then
+    rm "doc/$file_name.aux"
+fi
+
+if [ -f "doc/$file_name.log" ]; then
+    rm "doc/$file_name.log"
+fi
+
+if [ -f "doc/$file_name.toc" ]; then
+    rm "doc/$file_name.toc"
+fi
