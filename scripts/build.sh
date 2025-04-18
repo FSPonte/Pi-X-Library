72058c6535b75432287ea5f60e2775a4748d5053
FSPonte <fsponte@outlook.com>
2025-04-18 21:34:17 +0100

    Added "" to the directory name

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@ if [ -f "build/pix_tester" ]; then
 	rm "build/pix_tester"
 fi
 
-cmake --build "build"
+cmake --build "build/"
 hexdump -C -v "build/pix_tester" > "logs/hexdump.txt"
 objdump -D "build/pix_tester" > "logs/objdump.txt"
 size -A "build/pix_tester" > "logs/size.txt"
