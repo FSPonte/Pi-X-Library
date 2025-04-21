fbce55722660c125f96843d31cc6872b97aafe66
FSPonte <fsponte@outlook.com>
2025-04-21 17:44:34 +0100

    Removed complexity

--- scripts/latex.sh
+++ scripts/latex.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
 
-echo -n "File name: "
-read file_name
-echo
-
-pdflatex -output-directory="doc/" doc/$file_name
-
-if [ -f "doc/texput.log" ]; then
-    rm "doc/texput.log"
-fi
-
-if [ -f "doc/$file_name.aux" ]; then
-    rm "doc/$file_name.aux"
-fi
-
-if [ -f "doc/$file_name.log" ]; then
-    rm "doc/$file_name.log"
-fi
-
-if [ -f "doc/$file_name.toc" ]; then
-    rm "doc/$file_name.toc"
-fi
+pdflatex -output-directory="doc/" "doc/manual.tex"
