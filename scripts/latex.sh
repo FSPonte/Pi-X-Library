107ec82e080c7ebd97914dc9ddec6c844bb6103a
FSPonte <fsponte@outlook.com>
2025-05-02 04:15:16 +0100

    Added safeguards

--- scripts/latex.sh
+++ scripts/latex.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-pdflatex -output-directory="doc/" "doc/manual.tex"
+if [ ! -d "docs/" ]; then
+	mkdir -p "docs/"
+fi
+
+if [ ! -d "docs/images/" ]; then
+	mkdir -p "docs/images/"
+fi
+
+if [ ! -d "docs/manual.tex" ]; then
+	touch "docs/manual.tex"
+fi
+
+pdflatex -output-directory="docs/" "docs/manual.tex"
