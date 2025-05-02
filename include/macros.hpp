bfea3bc6df74383679f50b8413f3d434ce68b970
FSPonte <fsponte@outlook.com>
2025-05-02 01:05:37 +0100

    Visual changes

--- include/macros.hpp
+++ include/macros.hpp
@@ -7,4 +7,25 @@
 // Default block allocation size
 #define D_BLOCK_ALLOC_SIZE 1000
 
+// Logger mode
+#define LOGGER_MODE true
+
+#if LOGGER_MODE
+
+	#include <logger.hpp>
+
+	#define LOGGER_INIT(file_path) logger _logger_(file_path)
+	#define LOGGER_LOG_MSG(msg) _logger_.log_msg(msg)
+	#define LOGGER_LOG_ARR(arr) _logger_.log_arr(arr)
+	#define LOGGER_LOG_AUG(mtx, vec) _logger_.log_arr(mtx, vec)
+
+#else
+
+	#define LOGGER_INIT(file_path)
+	#define LOGGER_LOG_MSG(msg)
+	#define LOGGER_LOG_ARR(arr)
+	#define LOGGER_LOG_AUG(mtx, vec)
+
+#endif // LOGGER_MODE
+
 #endif // _MACROS_HPP_
--- include/pix_lib.hpp
+++ include/pix_lib.hpp
@@ -1,26 +1,6 @@
 #ifndef _PIX_LIB_HPP_
 #define _PIX_LIB_HPP_
 
-#define LOGGER_MODE true
-
-#if LOGGER_MODE
-
-	#include <logger.hpp>
-
-	#define LOGGER_INIT(file_path) logger _logger_(file_path)
-	#define LOGGER_LOG_MSG(msg) _logger_.log_msg(msg)
-	#define LOGGER_LOG_ARR(arr) _logger_.log_arr(arr)
-	#define LOGGER_LOG_AUG(mtx, vec) _logger_.log_arr(mtx, vec)
-
-#else
-
-	#define LOGGER_INIT(file_path)
-	#define LOGGER_LOG_MSG(msg)
-	#define LOGGER_LOG_ARR(arr)
-	#define LOGGER_LOG_AUG(mtx, vec)
-
-#endif // LOGGER_MODE
-
 #include <sys_vars.hpp>
 #include <types.hpp>
 #include <constants.hpp>
