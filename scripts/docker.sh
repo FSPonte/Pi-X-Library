c24705b39bc42d409cab11ff1139b1d6aed76941
FSPonte <fsponte@outlook.com>
2025-05-03 02:16:48 +0100

    Added logging for the output

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p "logs/"
+
 # Check if a container with the name "pixlib" already exists
 if [ "$(docker ps -aq -f name=pixlib)" ]; then
     docker container stop pixlib > /dev/null 2>&1
@@ -7,7 +9,7 @@ if [ "$(docker ps -aq -f name=pixlib)" ]; then
 fi
 
 # Build the image
-docker image build -t pixlib ./
+docker image build -t pixlib "./" 2>&1 | tee logs/docker.txt
 
 # Create a new container
 docker container create --name pixlib pixlib:latest > /dev/null 2>&1
