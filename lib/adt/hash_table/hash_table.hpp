e1211b0d620886efc966315f3cc796a64b879724
FSPonte <fsponte@outlook.com>
2025-05-05 01:25:51 +0100

    Visual changes

--- lib/adt/hash_table/hash_table.hpp
+++ lib/adt/hash_table/hash_table.hpp
@@ -10,11 +10,11 @@ namespace pix::adt
 	template <typename type_t, unsigned long BUFFER_SIZE = D_BUFFER_SIZE>
 	struct hash_item
 	{
-		char key[BUFFER_SIZE];
+		char _key[BUFFER_SIZE];
 		type_t _value;
 	};
 
-	template <typename type_t, unsigned long BUFFER_SIZE = D_BUFFER_SIZE, unsigned long DIM>
+	template <typename type_t, unsigned long DIM, unsigned long BUFFER_SIZE = D_BUFFER_SIZE>
 	class hash_table
 	{
 	private:
