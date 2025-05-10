61087fa39b75b33f584910fb221a7117641b969e
FSPonte <fsponte@outlook.com>
2025-05-10 02:40:56 +0100

    Moved declarations around

--- include/asserts.hpp
+++ include/asserts.hpp
@@ -1,6 +1,9 @@
 #ifndef _ASSERTS_HPP_
 #define _ASSERTS_HPP_
 
+// Dependencies
+#include <types.hpp>
+
 /**
  * @brief Assert pointer is not null
  * @param ptr Pointer
@@ -14,5 +17,50 @@
 */
 #define assert_not_nulldim(dim) \
 	static_assert(dim != 0, "Dimension is null")
+
+/**
+ * @brief Static assertion for is_same
+ * @param type_1 Data type 1
+ * @param type_2 Data type 2
+*/
+#define is_same_static_assert(type_1, type_2) \
+static_assert                                 \
+(                                             \
+	is_same(type_1, type_2),                  \
+	"Not the same data type"                  \
+)
+
+/**
+ * @brief Static assertion for is_number
+ * @param type Data type
+*/
+#define is_number_static_assert(type)   \
+static_assert                           \
+(                                       \
+	is_number(type),                    \
+	"Non primitive numerical data type" \
+)
+
+/**
+ * @brief Static assertion for is_integer
+ * @param type Data type
+*/
+#define is_integer_static_assert(type) \
+static_assert                          \
+(                                      \
+	is_integer(type),                  \
+	"Non primitive integer data type"  \
+)
+
+/**
+ * @brief Static assertion for is_float
+ * @param type Data type
+*/
+#define is_float_static_assert(type) \
+static_assert                        \
+(                                    \
+	is_float(type),                  \
+	"Non primitive float data type"  \
+)
 
 #endif // _ASSERTS_HPP_
--- include/types.hpp
+++ include/types.hpp
@@ -1,6 +1,35 @@
 #ifndef _TYPES_HPP_
 #define _TYPES_HPP_
 
+/**
+ * @brief Comparison between two data types
+ * @param type_1 Data type 1
+ * @param type_2 Data type 2
+ * @return True if arguments have the same data type
+*/
+#define is_same(type_1, type_2) pix::types::is_same<type_1, type_2>::VALUE
+
+/**
+ * @brief Checks type for primitive numerical data type
+ * @param type Data type
+ * @return True if is a primitive numerical data type
+*/
+#define is_number(type) pix::types::is_number<type>::VALUE
+
+/**
+ * @brief Checks type for primitive integer numerical data type
+ * @param type Data type
+ * @return True if is a primitive integer numerical data type
+*/
+#define is_integer(type) pix::types::is_integer<type>::VALUE
+
+/**
+ * @brief Checks type for primitive floating point numerical data type
+ * @param type Data type
+ * @return True if is a primitive floating point numerical data type
+*/
+#define is_float(type) pix::types::is_float<type>::VALUE
+
 namespace pix::types
 {
 	typedef unsigned char byte_t;
@@ -15,6 +44,156 @@ namespace pix::types
 	typedef long long int64_t;
 	typedef unsigned long long uint64_t;
 	typedef long double double_t;
+
+	template <typename type_t_1, typename type_t_2>
+	struct is_same
+	{
+		static constexpr const bool VALUE = false;
+	};
+
+	template <typename type_t>
+	struct is_same<type_t, type_t>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <typename type_t>
+	struct is_number
+	{
+		static constexpr const bool VALUE = false;
+	};
+
+	template <>
+	struct is_number<short>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_number<unsigned short>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_number<int>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_number<unsigned int>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_number<long>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_number<unsigned long>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_number<long long>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_number<unsigned long long>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_number<float>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_number<double>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_number<long double>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <typename type_t>
+	struct is_integer
+	{
+		static constexpr const bool VALUE = false;
+	};
+
+	template <>
+	struct is_integer<short>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_integer<unsigned short>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_integer<int>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_integer<unsigned int>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_integer<long>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_integer<unsigned long>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <typename type_t>
+	struct is_float
+	{
+		static constexpr const bool VALUE = false;
+	};
+
+	template <>
+	struct is_float<float>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_float<double>
+	{
+		static constexpr const bool VALUE = true;
+	};
+
+	template <>
+	struct is_float<long double>
+	{
+		static constexpr const bool VALUE = true;
+	};
 }
 
 #endif // _TYPES_HPP_
