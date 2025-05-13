185c15514e15366db6cecc60e5df4145054c2ebe
FSPonte <fsponte@outlook.com>
2025-05-13 10:34:26 +0100

    Simplified the exceptions

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -4,29 +4,15 @@
 // Exceptions
 namespace pix::exceptions
 {
-	enum id
-	{
-		null_dim, // Null dimension
-		null_ptr, // Null pointer
-		bad_alloc, // Bad memory allocation
-		underflow, // Numeric underflow
-		overflow, // Numeric overflow
-		bad_range, // Bad numeric range
-		div_zero, // Division by zero
-		inv_arg // Invalid argument
-	};
-
-	constexpr const char* msg[] =
-	{
-		"Null dimension",
-		"Null pointer",
-		"Bad memory allocation",
-		"Numeric underflow",
-		"Numeric overflow",
-		"Bad numeric range",
-		"Division by zero",
-		"Invalid argument"
-	};
+	constexpr const char
+		null_dim[] = "Null dimension",
+		null_ptr[] = "Null pointer",
+		bad_alloc[] = "Bad memory allocation",
+		underflow[] = "Numeric underflow",
+		overflow[] = "Numeric overflow",
+		bad_range[] = "Bad numeric range",
+		div_zero[] = "Division by zero",
+		inv_arg[] = "Invalid argument";
 }
 
 #endif // _EXCEPTIONS_HPP_
