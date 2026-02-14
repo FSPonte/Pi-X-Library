86dcccc1b70243029003fe1470114e2b786b091a
FSPonte <fsponte@outlook.com>
2026-02-14 09:49:25 -0100

    Reordering of the instructions

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,16 +1,16 @@
 sudo -l
 
-echo "\n\tRemoving unused dependency packages"
-sudo apt autoremove -y
-
-echo "\n\tFixing broken dependencies"
-sudo apt install -f
-
 echo "\n\tUpdating the package index"
 sudo apt update
 
 echo "\n\tInstalling available upgrades"
 sudo apt full-upgrade -y
+
+echo "\n\tRemoving unused dependency packages"
+sudo apt autoremove -y
+
+echo "\n\tFixing broken dependencies"
+sudo apt install -f
 
 echo "\n\tRemoving package cache"
 sudo apt autoclean
