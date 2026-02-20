db09c93e23911e48c9f1d37ed6a404581a4ae85a
FSPonte <fsponte@outlook.com>
2026-02-20 14:40:30 +0000

    Added a exception for lack of OS support

--- include/exceptions.hpp
+++ include/exceptions.hpp
@@ -13,7 +13,8 @@ namespace pix::exceptions
 		bad_range[] = "Bad numeric range",
 		div_zero[] = "Division by zero",
 		inv_arg[] = "Invalid argument",
-		out_of_bounds[] = "Out of bounds";
+		out_of_bounds[] = "Out of bounds",
+		os_not_supported[] = "OS not supported";
 }
 
 #endif // _EXCEPTIONS_HPP_
