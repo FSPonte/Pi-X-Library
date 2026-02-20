0846d09ff167293479769440272f2e4c1dec849a
FSPonte <fsponte@outlook.com>
2026-02-20 14:44:03 +0000

    Changed the names of the macros

--- include/os.hpp
+++ include/os.hpp
@@ -1,8 +1,8 @@
 #ifndef _OS_HPP_
 #define _OS_HPP_
 
-#define OS_LINUX defined(__linux__) // Linux
-#define OS_WIN (defined(_WIN32) || defined(_WIN64)) // Windows
-#define OS_APPLE defined(__APPLE__) // MacOS
+#define LINUX_DEFINED defined(__linux__) // Linux
+#define WIN_DEFINED (defined(_WIN32) || defined(_WIN64)) // Windows
+#define APPLE_DEFINED defined(__APPLE__) // MacOS
 
 #endif // _OS_HPP_
