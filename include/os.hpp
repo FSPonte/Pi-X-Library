25cb8d01580dad6746b7f9b8a636bbf657f6278e
FSPonte <fsponte@outlook.com>
2026-02-20 14:50:05 +0000

    Eliminated the undefined behavior in the macro double pass

--- include/os.hpp
+++ include/os.hpp
@@ -1,8 +1,22 @@
 #ifndef _OS_HPP_
 #define _OS_HPP_
 
-#define LINUX_DEFINED defined(__linux__) // Linux
-#define WIN_DEFINED (defined(_WIN32) || defined(_WIN64)) // Windows
-#define APPLE_DEFINED defined(__APPLE__) // MacOS
+#if defined(__linux__)
+    #define LINUX_DEFINED 1 // Linux
+#else
+    #define LINUX_DEFINED 0 // Linux
+#endif
+
+#if defined(_WIN32) || defined(_WIN64)
+    #define WIN_DEFINED 1 // Windows
+#else
+    #define WIN_DEFINED 0 // Windows
+#endif
+
+#if defined(__APPLE__)
+    #define APPLE_DEFINED 1 // MacOS
+#else
+    #define APPLE_DEFINED 0 // MacOS
+#endif
 
 #endif // _OS_HPP_
