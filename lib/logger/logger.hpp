3e66a296543b6c446dce6bec6bb4c18c0fb16343
FSPonte <fsponte@outlook.com>
2026-02-24 14:33:38 +0000

    Put the logger in the pix namespace

--- lib/logger/logger.hpp
+++ lib/logger/logger.hpp
@@ -6,105 +6,108 @@
 #include <string>
 #include <mutex>
 
-constexpr const unsigned long MAX_SESSION_ID = 1E3; // Maximum number of sessions
-
-/**
- * @brief Logger class
-*/
-class logger
+namespace pix
 {
-public:
+	constexpr unsigned long MAX_SESSION_ID = 1E3; // Maximum number of sessions
 
 	/**
-	 * @brief Constructor
-	 * @param file_path Path to the log file
-	 * @throw Failed to open file
+	 * @brief Logger class
 	*/
-	explicit logger(const std::string&) noexcept(false);
+	class logger
+	{
+	public:
 
-	// Disable copy constructor
-	logger(const logger&) = delete;
+		/**
+		 * @brief Constructor
+		 * @param file_path Path to the log file
+		 * @throw Failed to open file
+		*/
+		explicit logger(const std::string&) noexcept(false);
 
-	/**
-	 * @brief Destructor
-	*/
-	~logger(void);
+		// Disable copy constructor
+		logger(const logger&) = delete;
 
-	/**
-	 * @brief Open a logging session
-	 * @throw File is not open
-	*/
-	void open_session(void) noexcept(false);
+		/**
+		 * @brief Destructor
+		*/
+		~logger(void);
 
-	/**
-	 * @brief Log a message
-	 * @param msg Message to log
-	 * @throw File is not open
-	*/
-	void log_msg(const std::string&) noexcept(false);
+		/**
+		 * @brief Open a logging session
+		 * @throw File is not open
+		*/
+		void open_session(void) noexcept(false);
 
-	/**
-	 * @brief Log an array as a list
-	 * @tparam type_t Data type
-	 * @param arr Array to log
-	 * @param dim Number of elements
-	 * @throw File is not open
-	*/
-	template <typename type_t>
-	void log_lst(const type_t[], unsigned long) noexcept(false);
+		/**
+		 * @brief Log a message
+		 * @param msg Message to log
+		 * @throw File is not open
+		*/
+		void log_msg(const std::string&) noexcept(false);
 
-	/**
-	 * @brief Log an array as a list
-	 * @tparam type_t Data type
-	 * @tparam DIM Number of elements
-	 * @param arr Array to log
-	 * @throw File is not open
-	*/
-	template <typename type_t, unsigned long DIM>
-	void log_lst(const type_t (&)[DIM]) noexcept(false);
+		/**
+		 * @brief Log an array as a list
+		 * @tparam type_t Data type
+		 * @param arr Array to log
+		 * @param dim Number of elements
+		 * @throw File is not open
+		*/
+		template <typename type_t>
+		void log_lst(const type_t[], unsigned long) noexcept(false);
 
-	/**
-	 * @brief Log an array
-	 * @tparam type_t Data type
-	 * @tparam DIM Number of elements
-	 * @param arr Array to log
-	 * @throw File is not open
-	*/
-	template <typename type_t, unsigned long DIM>
-	void log_arr(const type_t (&)[DIM]) noexcept(false);
+		/**
+		 * @brief Log an array as a list
+		 * @tparam type_t Data type
+		 * @tparam DIM Number of elements
+		 * @param arr Array to log
+		 * @throw File is not open
+		*/
+		template <typename type_t, unsigned long DIM>
+		void log_lst(const type_t (&)[DIM]) noexcept(false);
 
-	/**
-	 * @brief Log an array
-	 * @tparam type_t Data type
-	 * @tparam N_LIN Number of lines
-	 * @tparam N_COL Number of columns
-	 * @param arr Array to log
-	 * @throw File is not open
-	*/
-	template <typename type_t, unsigned long N_LIN, unsigned long N_COL>
-	void log_arr(const type_t (&)[N_LIN][N_COL]) noexcept(false);
+		/**
+		 * @brief Log an array
+		 * @tparam type_t Data type
+		 * @tparam DIM Number of elements
+		 * @param arr Array to log
+		 * @throw File is not open
+		*/
+		template <typename type_t, unsigned long DIM>
+		void log_arr(const type_t (&)[DIM]) noexcept(false);
 
-	/**
-	 * @brief Log two arrays
-	 * @tparam type_t Data type
-	 * @tparam N_LIN Number of lines
-	 * @tparam N_COL Number of columns
-	 * @param arr_1 Array 1 to log
-	 * @param arr_2 Array 2 to log
-	 * @throw File is not open
-	*/
-	template <typename type_t, unsigned long N_LIN, unsigned long N_COL>
-	void log_arr(const type_t (&)[N_LIN][N_COL], const type_t (&)[N_LIN]) noexcept(false);
+		/**
+		 * @brief Log an array
+		 * @tparam type_t Data type
+		 * @tparam N_LIN Number of lines
+		 * @tparam N_COL Number of columns
+		 * @param arr Array to log
+		 * @throw File is not open
+		*/
+		template <typename type_t, unsigned long N_LIN, unsigned long N_COL>
+		void log_arr(const type_t (&)[N_LIN][N_COL]) noexcept(false);
 
-	// Disable assignment operator
-	logger& operator = (const logger&) = delete;
+		/**
+		 * @brief Log two arrays
+		 * @tparam type_t Data type
+		 * @tparam N_LIN Number of lines
+		 * @tparam N_COL Number of columns
+		 * @param arr_1 Array 1 to log
+		 * @param arr_2 Array 2 to log
+		 * @throw File is not open
+		*/
+		template <typename type_t, unsigned long N_LIN, unsigned long N_COL>
+		void log_arr(const type_t (&)[N_LIN][N_COL], const type_t (&)[N_LIN]) noexcept(false);
 
-private:
+		// Disable assignment operator
+		logger& operator = (const logger&) = delete;
 
-	unsigned long _session_id; // Session id number
-	std::ofstream _file; // File
-	std::mutex _mutex;
-};
+	private:
+
+		unsigned long _session_id; // Session id number
+		std::ofstream _file; // File
+		std::mutex _mutex;
+	};
+}
 
 // Template file
 #include "logger.tpp"
