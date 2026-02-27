be58196a22cb4fa2fb1cb6054398df7304fdea8b
FSPonte <fsponte@outlook.com>
2026-02-27 22:35:55 +0000

    Removed the macros

--- include/macros.hpp
+++ include/macros.hpp
@@ -1,7 +1,4 @@
 #ifndef _MACROS_HPP_
 #define _MACROS_HPP_
 
-#define TRACEBACK pix::traceback::add(__FILE__, __FUNCTION__, __LINE__)
-#define LOGGER_ENABLED true // Logger is enabled
-
 #endif // _MACROS_HPP_
