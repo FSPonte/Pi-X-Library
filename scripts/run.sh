7f8afe600041c9aecf9acf68c4c7bf951a9db3cb
FSPonte <fsponte@outlook.com>
2026-03-04 01:10:07 +0000

    Removed the pipe to a text file

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,5 @@ sh "scripts/build.sh"
 mkdir -p "files/"
 mkdir -p "logs/"
 
-/usr/bin/time -v "build/pix_tester" | tee "logs/stdout.txt"
+/usr/bin/time -v "build/pix_tester"
 gnuplot "scripts/plot.gp"
