96fe56dd37afa0189cf3f19be395caed329bdafa
FSPonte <fsponte@outlook.com>
2026-03-05 21:02:15 +0000

    Replaced a reference for a dereference to fix a bug in the sizeof a data type

--- include/macros.hpp
+++ include/macros.hpp
@@ -2,7 +2,7 @@
 #define _MACROS_HPP_
 
 #define symbol_name(symbol) #symbol
-#define array_size(arr) (sizeof(arr) / sizeof(&arr))
+#define array_size(arr) (sizeof(arr) / sizeof(*arr))
 
 #define TRACEBACK_ENABLED true
 #define LOGGER_ENABLED true
