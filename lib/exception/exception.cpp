f06e9c0c0545f782cd58e2e3083a528600037f0f
FSPonte <fsponte@outlook.com>
2026-03-05 16:58:41 +0000

    Better visual structure and compatibility with thetraceback labrary

--- lib/exception/exception.cpp
+++ lib/exception/exception.cpp
@@ -1,29 +1,49 @@
 // Header file
 #include "exception.hpp"
 
+// Dependencies
+#include <iostream>
+
+static constexpr char
+	RESET[] = "\033[0m",
+	BLUE[] = "\033[34m";
+
 namespace pix::exception
 {
-	excep::excep(const std::string& name, traceback<true>* const route) noexcept(true) : _name(name), _route(route)
+	excep::excep(const std::string& name, traceback<TRACEBACK_ENABLED>* const route) noexcept(true) : _name(name), _route(route)
 	{}
 
 	std::string excep::what(void) const noexcept(true)
 	{
-		std::string ret = "[Exception] " + this->_name;
-
-		if (this->_route != nullptr)
-			ret += this->_route->get();
-
-		return ret;
+		return std::string(
+			"\n[" + std::string(BLUE) + "Exception" + RESET + "] " +
+			this->_name + '\n'
+		);
 	}
 
-	null_dim::null_dim(traceback<true>* route) noexcept(true) : excep("Null dimension", route) {}
-	null_ptr::null_ptr(traceback<true>* route) noexcept(true) : excep("Null pointer", route) {}
-	bad_alloc::bad_alloc(traceback<true>* route) noexcept(true) : excep("Bad memory allocation", route) {}
-	underflow::underflow(traceback<true>* route) noexcept(true) : excep("Numeric underflow", route) {}
-	overflow::overflow(traceback<true>* route) noexcept(true) : excep("Numerica overflow", route) {}
-	bad_range::bad_range(traceback<true>* route) noexcept(true) : excep("Bad numeric range", route) {}
-	div_zero::div_zero(traceback<true>* route) noexcept(true) : excep("Division by zero", route) {}
-	invalid_argument::invalid_argument(traceback<true>* route) noexcept(true) : excep("Invalid argument", route) {}
-	out_of_bounds::out_of_bounds(traceback<true>* route) noexcept(true) : excep("Out of bounds", route) {}
-	os_not_supported::os_not_supported(traceback<true>* route) noexcept(true) : excep("OS is not supported", route) {}
+	void excep::print(void) const noexcept(true)
+	{
+		std::cout
+			<< "\n["
+			<< BLUE
+			<< "Exception"
+			<< RESET
+			<< "] "
+			<< this->_name
+			<< '\n';
+
+		if (this->_route != nullptr)
+			this->_route->print();
+	}
+
+	null_dim::null_dim(traceback<TRACEBACK_ENABLED>* route) noexcept(true) : excep("Null dimension", route) {}
+	null_ptr::null_ptr(traceback<TRACEBACK_ENABLED>* route) noexcept(true) : excep("Null pointer", route) {}
+	bad_alloc::bad_alloc(traceback<TRACEBACK_ENABLED>* route) noexcept(true) : excep("Bad memory allocation", route) {}
+	underflow::underflow(traceback<TRACEBACK_ENABLED>* route) noexcept(true) : excep("Numeric underflow", route) {}
+	overflow::overflow(traceback<TRACEBACK_ENABLED>* route) noexcept(true) : excep("Numerica overflow", route) {}
+	bad_range::bad_range(traceback<TRACEBACK_ENABLED>* route) noexcept(true) : excep("Bad numeric range", route) {}
+	div_zero::div_zero(traceback<TRACEBACK_ENABLED>* route) noexcept(true) : excep("Division by zero", route) {}
+	invalid_argument::invalid_argument(traceback<TRACEBACK_ENABLED>* route) noexcept(true) : excep("Invalid argument", route) {}
+	out_of_bounds::out_of_bounds(traceback<TRACEBACK_ENABLED>* route) noexcept(true) : excep("Out of bounds", route) {}
+	os_not_supported::os_not_supported(traceback<TRACEBACK_ENABLED>* route) noexcept(true) : excep("OS is not supported", route) {}
 }
--- lib/exception/exception.hpp
+++ lib/exception/exception.hpp
@@ -1,5 +1,5 @@
-#ifndef _EXCEPTION_HPP_
-#define _EXCEPTION_HPP_
+#ifndef __EXCEPTION_HPP__
+#define __EXCEPTION_HPP__
 
 // Dependencies
 #include <traceback.hpp>
@@ -11,84 +11,85 @@ namespace pix::exception
 	{
 	public:
 
-		excep(const std::string&, traceback<true>* = nullptr) noexcept(true);
+		excep(const std::string&, traceback<TRACEBACK_ENABLED>* = nullptr) noexcept(true);
 		std::string what(void) const noexcept(true);
+		void print(void) const noexcept(true);
 
 	protected:
 
 		const std::string _name;
-		traceback<true>* _route;
+		traceback<TRACEBACK_ENABLED>* _route;
 	};
 
 	class null_dim : public excep
 	{
 	public:
 
-		null_dim(traceback<true>* = nullptr) noexcept(true);
+		null_dim(traceback<TRACEBACK_ENABLED>* = nullptr) noexcept(true);
 	};
 
 	class null_ptr : public excep
 	{
 	public:
 		
-		null_ptr(traceback<true>* = nullptr) noexcept(true);
+		null_ptr(traceback<TRACEBACK_ENABLED>* = nullptr) noexcept(true);
 	};
 
 	class bad_alloc : public excep
 	{
 	public:
 		
-		bad_alloc(traceback<true>* = nullptr) noexcept(true);
+		bad_alloc(traceback<TRACEBACK_ENABLED>* = nullptr) noexcept(true);
 	};
 	
 	class underflow : public excep
 	{
 	public:
 		
-		underflow(traceback<true>* = nullptr) noexcept(true);
+		underflow(traceback<TRACEBACK_ENABLED>* = nullptr) noexcept(true);
 	};
 
 	class overflow : public excep
 	{
 	public:
 		
-		overflow(traceback<true>* = nullptr) noexcept(true);
+		overflow(traceback<TRACEBACK_ENABLED>* = nullptr) noexcept(true);
 	};
 
 	class bad_range : public excep
 	{
 	public:
 		
-		bad_range(traceback<true>* = nullptr) noexcept(true);
+		bad_range(traceback<TRACEBACK_ENABLED>* = nullptr) noexcept(true);
 	};
 	
 	class div_zero : public excep
 	{
 	public:
 		
-		div_zero(traceback<true>* = nullptr) noexcept(true);
+		div_zero(traceback<TRACEBACK_ENABLED>* = nullptr) noexcept(true);
 	};
 
 	class invalid_argument : public excep
 	{
 	public:
 		
-		invalid_argument(traceback<true>* = nullptr) noexcept(true);
+		invalid_argument(traceback<TRACEBACK_ENABLED>* = nullptr) noexcept(true);
 	};
 	
 	class out_of_bounds : public excep
 	{
 	public:
 		
-		out_of_bounds(traceback<true>* = nullptr) noexcept(true);
+		out_of_bounds(traceback<TRACEBACK_ENABLED>* = nullptr) noexcept(true);
 	};
 	
 	class os_not_supported : public excep
 	{
 	public:
 		
-		os_not_supported(traceback<true>* = nullptr) noexcept(true);
+		os_not_supported(traceback<TRACEBACK_ENABLED>* = nullptr) noexcept(true);
 	};
 }
 
-#endif // _EXCEPTION_HPP_
+#endif // __EXCEPTION_HPP__
