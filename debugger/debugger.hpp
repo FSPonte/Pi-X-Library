f5d2e29db64af3381b0b084a959350d99bafdf02
FSPonte <fsponte@outlook.com>
2026-03-05 23:59:27 +0000

    Added debug macros

--- debugger/debugger.hpp
+++ debugger/debugger.hpp
@@ -2,7 +2,9 @@
 #define __DEBUGGER_HPP__
 
 // Dependencies
-#include <pix_lib.hpp>
+#include <traceback.hpp>
+#include <logger.hpp>
+#include <tester.hpp>
 
 namespace debugger
 {
@@ -10,5 +12,14 @@ namespace debugger
 	extern pix::logger<LOGGER_ENABLED> __logger__;
 	extern pix::tester __tester__;
 }
+
+#define DEBUG_TRACEBACK_INIT debugger::__route__.init(__FUNCTION__, __FILE__, __LINE__)
+#define DEBUG_TRACEBACK_TRIG debugger::__route__.trigger(__LINE__)
+#define DEBUG_TRACEBACK_RT debugger::__route__
+
+#define DEBUG_TESTER_INIT debugger::__tester__.init(__FUNCTION__)
+#define DEBUG_TESTER_START debugger::__tester__.start(__FUNCTION__, __FILE__)
+#define DEBUG_TESTER_PASS debugger::__tester__.pass(__LINE__)
+#define DEBUG_TESTER_FAIL debugger::__tester__.fail(__LINE__)
 
 #endif // __DEBUGGER_HPP__
